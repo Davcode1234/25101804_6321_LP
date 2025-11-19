ee22ab85e9cec6daf5979479c6fb765f9d13da49
Davcode1234 <dpietruszewski12@gmail.com>
2025-11-19 09:19:56 +0100

    finish desktop form view

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const textToWrap = document.querySelector(".expanded");
-const mainSec = document.querySelector(".main-sec-container");
-const infoSection = document.querySelector(".info-section");
-const listColumn = document.querySelector(".list-column");
+const backgroundImg = document.querySelector(".background-img");
+const rightColumn = document.querySelector(".right-column");
+const mainWrapper = document.querySelector(".main-wrapper");
+const titleFlagContainer = document.querySelector(".title-flag-container");
 
 toggleBtn.addEventListener("click", (e) => {
   e.preventDefault();
   textToWrap.classList.toggle("expanded");
+  backgroundImg.classList.toggle("extended-background-img");
+  rightColumn.classList.toggle("extended-right-column");
+  mainWrapper.classList.toggle("extended-main-wrapper");
+  titleFlagContainer.classList.toggle("extended-title-flag-container");
   if (window.matchMedia("(min-width: 1300px)").matches) {
     infoSection.classList.toggle("expanded");
   } else if (window.matchMedia("(max-width: 1300px)").matches) {
@@ -15,18 +20,17 @@ toggleBtn.addEventListener("click", (e) => {
 
   listColumn.classList.toggle("expanded");
 
-  mainSec.classList.toggle("main-sec-expanded-form");
   const open = textToWrap.classList.contains("expanded");
   toggleBtn.textContent = open ? "Zwiń" : "Rozwiń";
 });
 
-function enforceInfoSection() {
-  if (window.matchMedia("(max-width: 1299px)").matches) {
-    infoSection.classList.remove("expanded");
-  } else {
-    infoSection.classList.add("expanded");
-  }
-}
+// function enforceInfoSection() {
+//   if (window.matchMedia("(max-width: 1299px)").matches) {
+//     infoSection.classList.remove("expanded");
+//   } else {
+//     infoSection.classList.add("expanded");
+//   }
+// }
 
 window.addEventListener("DOMContentLoaded", enforceInfoSection);
 
